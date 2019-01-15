6026c8794dde9b30999c55db7a8c424c6ffd3ee7
nireus <nireus@ixiaopu.com>
2019-01-15 18:00:54 +0800

    Initial commit

---
+++ react-native/demo01/src/home.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { increase, decrease, reset } from './models/action';
+
+type Props = {};
+class Home extends Component<Props> {
+  _onPressReset () {
+    this.props.dispatch(reset());
+  }
+
+  _onPressInc () {
+    this.props.dispatch(increase());
+  }
+
+  _onPressDec () {
+    this.props.dispatch(decrease());
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.counter}>{this.props.counter.count}</Text>
+        <TouchableOpacity style={styles.reset} onPress={()=>this._onPressReset()}>
+          <Text>归零</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.start} onPress={()=>this._onPressInc()}>
+          <Text>加1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.stop} onPress={()=>this._onPressDec()}>
+          <Text>减1</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  counter: {
+    fontSize: 50,
+    marginBottom: 70
+  },
+  reset: {
+    margin: 10,
+    backgroundColor: 'yellow'
+  },
+  start: {
+    margin: 10,
+    backgroundColor: 'yellow'
+  },
+  stop: {
+    margin: 10,
+    backgroundColor: 'yellow'
+  }
+});
+
+const mapStateToProps = state => ({
+  counter: state.counter
+});
+
+export default connect(mapStateToProps)(Home);
