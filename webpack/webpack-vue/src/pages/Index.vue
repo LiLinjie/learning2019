889244f74b8ea2adc9383d17964c2f0c5373c29a
lilinjie <linda1989@yeah.net>
2019-05-06 10:46:45 +0800

    webpack vue

--- webpack/webpack-vue/src/pages/Index.vue
+++ webpack/webpack-vue/src/pages/Index.vue
@@ -3,9 +3,7 @@
     <!--<img :src="bg" alt="">-->
     <img :src="icon" alt="">
     <img src="/static/images/icon.png" alt="">
-    <p>做过移动端需求的前端肯定是避免不了处理 1px 细线问题，这个问题的原因就是 UI 对页面美观度的要求越来越高（不要和我说这是 retina 屏的问题）。
-
-      据小生所知好像没有什么兼容性特别好的方案，这里我只是提供两种种相对较好的方案。</p>
+    <p>做过移动端需求的前端肯定是避免不了处理 1px 细线问题，这个问题的原因就是 UI 对页面美观度的要求越来越高（不要和我说这是 retina 屏的问题）。</p>
   </div>
 </template>
 
@@ -42,6 +40,6 @@ export default {
   }
   p {
     width: 100px;
-    .line-camp();
+    .line-camp(3);
   }
 </style>
