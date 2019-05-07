d275452f4178c0c76b689d8a39a190dd384ae8bf
lilinjie <linda1989@yeah.net>
2019-05-07 11:33:04 +0800

    react-native demo2 init

--- react-native/demo2/index.ios.js
+++ react-native/demo2/index.ios.js
@@ -7,7 +7,6 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
 } from 'react-native';
 import App from './src/App'
 
@@ -18,24 +17,5 @@ export default class demo2 extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('demo2', () => demo2);
