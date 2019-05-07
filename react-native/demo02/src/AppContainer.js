86242f5d51c2226462d478b7643d171cc5372b34
lilinjie <linda1989@yeah.net>
2019-05-07 16:21:55 +0800

    react-native demo02 react-navigation 跳转

--- react-native/demo02/src/AppContainer.js
+++ react-native/demo02/src/AppContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class HomeScreen extends React.Component {
   render () {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     )
   }
@@ -17,6 +21,10 @@ class DetailsScreen extends React.Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => this.props.navigation.push('Details')}
+        />
       </View>
     )
   }
